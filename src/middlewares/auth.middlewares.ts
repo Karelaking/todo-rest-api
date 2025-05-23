8873cdf5c91af9b5e2c4db996234a9573d598220
mradul kumar katiyar <dev.mradul@proton.me>
2025-05-23 22:22:21 +0530

    feat: added auth middlewares

---
+++ src/middlewares/auth.middlewares.ts
@@ -0,0 +1,43 @@
+import type { IUser } from "@/types/user.types";
+import type { Request, NextFunction } from "express";
+import type { IRequest } from "@/types/request.type";
+
+import jwt from "jsonwebtoken";
+import { config } from "@/config";
+import { User } from "@/models/user.model";
+import { ApiError } from "@/utils/apiError";
+import { asyncHandler } from "@/utils/asyncHandler";
+
+const { jwtSecret } = config.jwtToken;
+
+// muddleware to protect routes
+export const authMiddleware = asyncHandler(
+  async (request: IRequest, _, next: NextFunction) => {
+    try {
+      const token =
+        request.headers.authorization?.replace("Bearer ", "") ||
+        request.cookies?.accessToken;
+      console.log("token", token);
+
+      if (!token) {
+        return next(new ApiError(401, "Token not found"));
+      }
+      const decoded = jwt.verify(token, jwtSecret) as IUser;
+
+      if (!decoded) {
+        return next(new ApiError(401, "Unable to decode token"));
+      }
+
+      const user = await User.findById(decoded?._id).select("-password");
+
+      if (!user) {
+        return next(new ApiError(401, "User not found"));
+      }
+
+      request.user = user as IUser;
+      next();
+    } catch (error: any) {
+      return next(new ApiError(401, error.message));
+    }
+  }
+);
